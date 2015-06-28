0cd17d132a5fc409d715d30b51e6b39c63ec6fb7
Hikigaya <otakusekainet@gmail.com>
2015-06-28 20:00:36 +0300

    Leona Interrupter + Ulti Fixed

--- HikiCarry Support/Properties/AssemblyInfo.cs
+++ HikiCarry Support/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HikiCarry Leona")]
+[assembly: AssemblyTitle("HikiCarry Support")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HikiCarry Leona")]
+[assembly: AssemblyProduct("HikiCarry Support")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3534e5fa-c81c-48db-9d47-d2595167d94a")]
+[assembly: Guid("7b2b7e93-0a51-48d6-85cb-409c330afd14")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
