af4c212797213aee234c0150be2e65c8be6aa323
Hikigaya <otakusekainet@gmail.com>
2015-11-24 22:47:09 +0200

    Illaoi: Passive Update

--- Illaoi - Tentacle Kitty/Program.cs
+++ Illaoi - Tentacle Kitty/Program.cs
@@ -187,7 +187,7 @@ namespace Illaoi___Tentacle_Kitty
             if (W.IsReady() && Config.Item("w.combo").GetValue<bool>())
             {
                 var tentacles =
-                    ObjectManager.Get<Obj_AI_Base>().Where(x => x.Name.ToLower().Contains("illaoiminion")).FirstOrDefault();
+                    ObjectManager.Get<Obj_AI_Minion>().Where(x => x.Name == "God").FirstOrDefault();
                 foreach (var enemy in HeroManager.Enemies.Where(x=> x.Distance(tentacles.Position) < Q.Range))
                 {
                     W.Cast();
