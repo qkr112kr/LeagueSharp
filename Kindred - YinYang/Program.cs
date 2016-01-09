60d3814d456c6f88701e0dcd97a8f1d1c1e73e83
Hikigaya <otakusekainet@gmail.com>
2016-01-09 22:53:56 +0200

    Kindred + REWORK + LANG PACK

--- Kindred - YinYang/Program.cs
+++ Kindred - YinYang/Program.cs
@@ -22,7 +22,6 @@ namespace Kindred___YinYang
         public static Vector3 OrderSpawnPosition = new Vector3(394, 461, 171);
         public static Vector3 ChaosSpawnPosition = new Vector3(14340, 14391, 179);
         public static Orbwalking.Orbwalker Orbwalker;
-
         public static string[] HighChamps =
             {
                 "Ahri", "Anivia", "Annie", "Ashe", "Azir", "Brand", "Caitlyn", "Cassiopeia", "Corki", "Draven",
@@ -37,209 +36,45 @@ namespace Kindred___YinYang
         }
         private static void Game_OnGameLoad(EventArgs args)
         {
-            if (Kindred.ChampionName != "Kindred")
-            {
-                return;
-            }
+            
 
             Q = new Spell(SpellSlot.Q, 340);
             W = new Spell(SpellSlot.W, 800);
             E = new Spell(SpellSlot.E, 500);
             R = new Spell(SpellSlot.R, 550);
 
+
             Config = new Menu("Kindred - Yin Yang", "Kindred - Yin Yang", true);
-            TargetSelector.AddToMenu(Config.SubMenu("Target Selector Settings"));
-            Orbwalker = new Orbwalking.Orbwalker(Config.SubMenu("Orbwalker Settings"));
-            var comboMenu = new Menu("Combo Settings", "Combo Settings");
             {
-                comboMenu.AddItem(new MenuItem("q.combo.style", "(Q) Combo Style").SetValue(new StringList(new[] {"Kite", "100% Hit","Safe Position"})));
-                comboMenu.AddItem(new MenuItem("q.combo", "Use (Q)").SetValue(true));
-                comboMenu.AddItem(new MenuItem("w.combo", "Use (W)").SetValue(true));
-                comboMenu.AddItem(new MenuItem("e.combo", "Use (E)").SetValue(true));
-                Config.AddSubMenu(comboMenu);
+                Orbwalker = new Orbwalking.Orbwalker(Config.SubMenu(":: Orbwalker Settings"));
+                Config.AddItem(new MenuItem("language.supx", "Language").SetValue(new StringList(new[] { "English", "Korean","Türkçe" }, 0)));
+                Language.MenuInit();
+                Config.AddToMainMenu();
             }
-            var eMenu = new Menu("(E) Settings", "(E) Settings");
-            {
-                eMenu.AddItem(new MenuItem("e.whte", "                     (E) Whitelist"));
-                foreach (var enemy in ObjectManager.Get<Obj_AI_Hero>().Where(o => o.IsEnemy))
-                {
-                    eMenu.AddItem(new MenuItem("enemy." + enemy.CharData.BaseSkinName, string.Format("E: {0}", enemy.CharData.BaseSkinName)).SetValue(HighChamps.Contains(enemy.CharData.BaseSkinName)));
-
-                }
-                Config.AddSubMenu(eMenu);
-            }
-            var harassMenu = new Menu("Harass Settings", "Harass Settings");
-            {
-                harassMenu.AddItem(new MenuItem("q.harass", "Use Q").SetValue(true));
-                harassMenu.AddItem(new MenuItem("w.harass", "Use W").SetValue(true));
-                harassMenu.AddItem(new MenuItem("e.harass", "Use E").SetValue(true));
-                harassMenu.AddItem(new MenuItem("harass.mana", "Mana Manager").SetValue(new Slider(20, 1, 99)));
-                Config.AddSubMenu(harassMenu);
-            }
-            var laneClear = new Menu("Clear Settings", "Clear Settings");
-            {
-                laneClear.AddItem(new MenuItem("q.clear", "Use Q").SetValue(true));
-                laneClear.AddItem(new MenuItem("q.minion.count", "Q Minion Count").SetValue(new Slider(4, 1, 5)));
-                laneClear.AddItem(new MenuItem("clear.mana", "Mana Manager").SetValue(new Slider(20, 1, 99)));
-                Config.AddSubMenu(laneClear);
-            }
-            var jungleClear = new Menu("Jungle Settings", "Jungle Settings");
-            {
-                jungleClear.AddItem(new MenuItem("q.jungle", "Use Q").SetValue(true));
-                jungleClear.AddItem(new MenuItem("w.jungle", "Use W").SetValue(true));
-                jungleClear.AddItem(new MenuItem("e.jungle", "Use E").SetValue(true));
-                jungleClear.AddItem(new MenuItem("jungle.mana", "Mana Manager").SetValue(new Slider(20, 1, 99)));
-                Config.AddSubMenu(jungleClear);
-            }
-            var ksMenu = new Menu("KillSteal Settings", "KillSteal Settings");
-            {
-                ksMenu.AddItem(new MenuItem("q.ks", "Use Q").SetValue(true));
-                ksMenu.AddItem(new MenuItem("q.ks.count", "Basic Attack Count").SetValue(new Slider(2, 1, 5)));
-                Config.AddSubMenu(ksMenu);
-            }
-            var miscMenu = new Menu("Miscellaneous", "Miscellaneous");
-            {
-                miscMenu.AddItem(new MenuItem("q.antigapcloser", "Anti-Gapcloser Q!").SetValue(true));
-                var antiRengar = new Menu("Anti Rengar", "Anti Rengar");
-                {
-                    antiRengar.AddItem(new MenuItem("anti.rengar", "Anti Rengar!").SetValue(true));
-                    antiRengar.AddItem(new MenuItem("hp.percent.for.rengar", "Min. HP Percent").SetValue(new Slider(30, 1, 99)));
-                    miscMenu.AddSubMenu(antiRengar);
-                }
-                var spellMenu = new Menu("Spell Breaker", "Spell Breaker");
-                {
-                    spellMenu.AddItem(new MenuItem("spell.broker", "Spell Breaker!").SetValue(true));
-                    spellMenu.AddItem(new MenuItem("katarina.r", "Katarina (R)").SetValue(true));
-                    spellMenu.AddItem(new MenuItem("missfortune.r", "Miss Fortune (R)").SetValue(true));
-                    spellMenu.AddItem(new MenuItem("lucian.r", "Lucian (R)").SetValue(true));
-                    spellMenu.AddItem(new MenuItem("hp.percent.for.broke", "Min. HP Percent").SetValue(new Slider(20, 1, 99)));
-                    miscMenu.AddSubMenu(spellMenu);
-                }
-                var rProtector = new Menu("(R) Protector", "(R) Protector");
-                {
-                    rProtector.AddItem(new MenuItem("protector","Disable Protector?").SetValue(true));
-                    foreach (var enemy in ObjectManager.Get<Obj_AI_Hero>().Where(o => o.IsEnemy))
-                    {
-                        foreach (var skillshot in SpellDatabase.Spells.Where(x => x.charName == enemy.ChampionName)) // 2.5F Protector
-                        {
-                            rProtector.AddItem(new MenuItem("hero." + skillshot.spellName, ""+skillshot.charName +"("+skillshot.spellKey+")").SetValue(true));
-                        }
-                    }
-                    miscMenu.AddSubMenu(rProtector);
-                }
-                Config.AddSubMenu(miscMenu);
-            }
-            var drawMenu = new Menu("Draw Settings", "Draw Settings");
-            {
-                var damageDraw = new Menu("Damage Draw", "Damage Draw");
-                {
-                    damageDraw.AddItem(new MenuItem("aa.indicator", "AA Indicator").SetValue(new Circle(true, Color.Gold)));
-                    drawMenu.AddSubMenu(damageDraw);
-                }
-                drawMenu.AddItem(new MenuItem("q.drawx", "Q Range").SetValue(new Circle(true, Color.White)));
-                drawMenu.AddItem(new MenuItem("w.draw", "W Range").SetValue(new Circle(true, Color.Gold)));
-                drawMenu.AddItem(new MenuItem("e.draw", "E Range").SetValue(new Circle(true, Color.DodgerBlue)));
-                drawMenu.AddItem(new MenuItem("r.draw", "R Range").SetValue(new Circle(true, Color.GreenYellow)));
-                Config.AddSubMenu(drawMenu);
-            }
-            Config.AddItem(new MenuItem("e.method", "E Method").SetValue(new StringList(new[] { "Cursor Position" })));
-            Config.AddItem(new MenuItem("use.r", "Use R").SetValue(true));
-            Config.AddItem(new MenuItem("r.whte", "                          R Whitelist"));
-            foreach (var ally in ObjectManager.Get<Obj_AI_Hero>().Where(o => o.IsAlly))
-            {
-                Config.AddItem(new MenuItem("respite." + ally.CharData.BaseSkinName, string.Format("Respite: {0}", ally.CharData.BaseSkinName)).SetValue(HighChamps.Contains(ally.CharData.BaseSkinName)));
-
-            }
-            Config.AddItem(new MenuItem("min.hp.for.r", "Min. HP Percent for R").SetValue(new Slider(20, 1, 99)));
-            Config.AddToMainMenu();
             Game.PrintChat("<font color='#ff3232'>Kindred - Yin Yang: </font> <font color='#d4d4d4'>If you like this assembly feel free to upvote on Assembly DB</font>");
             Obj_AI_Base.OnProcessSpellCast += OnProcessSpellCast;
             AntiGapcloser.OnEnemyGapcloser += AntiGapcloser_OnEnemyGapcloser;
             Game.OnUpdate += Game_OnGameUpdate;
             GameObject.OnCreate += GameObject_OnCreate;
             Drawing.OnDraw += Drawing_OnDraw;
+            Obj_AI_Base.OnDoCast += OnDoCast;
         }
 
-       
+        private static void OnDoCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
+        {
+            Helper.OnDoCast(sender,args);
+        }
         private static void OnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs spell)
         {
-            if (!R.IsReady() && Kindred.IsDead && Kindred.IsZombie && sender.IsAlly && !sender.IsMe && !Config.Item("protector").GetValue<bool>())
-            {
-                return;
-            }
-            if (sender is Obj_AI_Hero && R.IsReady() && sender.IsEnemy && !spell.SData.IsAutoAttack()
-                && !sender.IsDead && !sender.IsZombie && sender.IsValidTarget(1000))
-            {
-                foreach (var protector in SpellDatabase.Spells.Where(x => x.spellName == spell.SData.Name 
-                    && Config.Item("hero." + x.spellName).GetValue<bool>()))
-                {
-                    if (protector.spellType == SpellType.Circular && Kindred.Distance(spell.End) <= 200 &&
-                        sender.GetSpellDamage(Kindred,protector.spellName) > Kindred.Health)
-                    {
-                        R.Cast(Kindred);
-                    }
-                    if (protector.spellType == SpellType.Cone && Kindred.Distance(spell.End) <= 200 &&
-                        sender.GetSpellDamage(Kindred, protector.spellName) > Kindred.Health)
-                    {
-                        R.Cast(Kindred);
-                    }
-                    if (protector.spellType == SpellType.Line && Kindred.Distance(spell.End) <= 200
-                        && sender.GetSpellDamage(Kindred,protector.spellName) > Kindred.Health)
-                    {
-                        Game.PrintChat("Killable");
-                        R.Cast(Kindred);
-                    }
-                }
-            }
+            Helper.Protector(sender,spell);
         }
         private static void GameObject_OnCreate(GameObject sender, EventArgs args)
         {
-            if (Config.Item("anti.rengar").GetValue<bool>() && R.IsReady() && sender.IsEnemy && !sender.IsAlly && !sender.IsDead
-                && sender.Name == "Rengar_LeapSound.troy" && Kindred.HealthPercent < Config.Item("hp.percent.for.rengar").GetValue<Slider>().Value)
-            {
-                foreach (var enemy in ObjectManager.Get<Obj_AI_Hero>().Where(x=> x.IsValidTarget(1000) && x.ChampionName == "Rengar"))
-                {
-                    R.Cast(Kindred);
-                }
-            }
-        }
-        private static void SpellBroker()
-        {
-            if (Config.Item("katarina.r").GetValue<bool>() && R.IsReady() && Kindred.HealthPercent < Config.Item("hp.percent.for.broke").GetValue<Slider>().Value)
-            {
-                foreach (var enemy in HeroManager.Enemies.Where(x => x.ChampionName == "Katarina" && x.IsValidTarget(R.Range) && x.HasBuff("katarinarsound") && !Kindred.IsDead && !x.IsDead && !x.IsZombie))
-                {
-                    R.Cast(Kindred);
-                }
-            }
-            if (Config.Item("lucian.r").GetValue<bool>() && R.IsReady() && Kindred.HealthPercent < Config.Item("hp.percent.for.broke").GetValue<Slider>().Value)
-            {
-                foreach (var enemy in HeroManager.Enemies.Where(x => x.ChampionName == "Lucian" && x.IsValidTarget(R.Range) && x.HasBuff("lucianr") && !Kindred.IsDead && !x.IsDead && !x.IsZombie))
-                {
-                    R.Cast(Kindred);
-                }
-            }
-            if (Config.Item("missfortune.r").GetValue<bool>() && R.IsReady() && Kindred.HealthPercent < Config.Item("hp.percent.for.broke").GetValue<Slider>().Value)
-            {
-                foreach (var enemy in HeroManager.Enemies.Where(x => x.ChampionName == "MissFortune" && x.IsValidTarget(R.Range) && x.HasBuff("missfortunebulletsound") && !Kindred.IsDead && !x.IsDead && !x.IsZombie))
-                {
-                    R.Cast(Kindred);
-                }
-            }
+            Helper.AntiRengarOnCreate(sender,args);
         }
         private static void AntiGapcloser_OnEnemyGapcloser(ActiveGapcloser gapcloser)
         {
-            if (gapcloser.End.Distance(ObjectManager.Player.ServerPosition) <= 300)
-            {
-                Q.Cast(gapcloser.End.Extend(ObjectManager.Player.ServerPosition, ObjectManager.Player.Distance(gapcloser.End) + Q.Range));
-            }
-        }
-        private static int AaIndicator(Obj_AI_Hero enemy)
-        {
-             double aCalculator = ObjectManager.Player.CalcDamage(enemy, Damage.DamageType.Physical, Kindred.TotalAttackDamage());
-             double killableAaCount = enemy.Health / aCalculator;
-             int totalAa = (int)Math.Ceiling(killableAaCount);
-             return totalAa;
+            Helper.AntiGapcloser(gapcloser);
         }
         private static void Game_OnGameUpdate(EventArgs args)
         {
@@ -260,7 +95,7 @@ namespace Kindred___YinYang
             }
             if (Config.Item("use.r").GetValue<bool>() && R.IsReady())
             {
-                RLogic();
+                Helper.ClassicUltimate();
             }
             if (Config.Item("q.ks").GetValue<bool>() && Q.IsReady())
             {
@@ -268,59 +103,14 @@ namespace Kindred___YinYang
             }
             if (Config.Item("spell.broker").GetValue<bool>() && R.IsReady())
             {
-                SpellBroker();
-            }
-           
-        }
-        private static void CollisionObjectCheckCast(Spell spell, Obj_AI_Hero unit, int count)
-        {
-            if (spell.GetPrediction(unit).CollisionObjects.Count <= count)
-            {
-                spell.Cast(Game.CursorPos);
-            }        
-        }
-        private static void CastSafePosition(Spell spell, Obj_AI_Hero hero)
-        {
-            if (Geometry.CircleCircleIntersection(ObjectManager.Player.ServerPosition.To2D(), Prediction.GetPrediction(hero, 0f, hero.AttackRange).UnitPosition.To2D(), spell.Range, Orbwalking.GetRealAutoAttackRange(hero)).Count() > 0)
-            {
-                spell.Cast(
-                    Geometry.CircleCircleIntersection(ObjectManager.Player.ServerPosition.To2D(),
-                        Prediction.GetPrediction(hero, 0f, hero.AttackRange).UnitPosition.To2D(), spell.Range,
-                        Orbwalking.GetRealAutoAttackRange(hero)).MinOrDefault(i => i.Distance(Game.CursorPos)));
-            }
-            else
-            {
-                spell.Cast(ObjectManager.Player.ServerPosition.Extend(hero.ServerPosition, -E.Range));
-            }
-        }
-        private static void QStyleCast(Spell spell, Obj_AI_Hero unit, int count)
-        {
-            switch (Config.Item("q.combo.style").GetValue<StringList>().SelectedIndex)
-            {
-                case 0:
-                    spell.Cast(Game.CursorPos);
-                    break;
-                case 1:
-                    CollisionObjectCheckCast(spell,unit,count);
-                    break;
-                case 2:
-                    CastSafePosition(spell,unit);
-                    break;
+                Helper.SpellBreaker();
             }
         }
         private static void Combo()
         {
-            var useQ = Config.Item("q.combo").GetValue<bool>();
             var useW = Config.Item("w.combo").GetValue<bool>();
             var useE = Config.Item("e.combo").GetValue<bool>();
 
-            if (useQ && Q.IsReady())
-            {
-                foreach (var enemy in HeroManager.Enemies.Where(o=> o.IsValidTarget(ObjectManager.Player.AttackRange) && !o.IsDead && !o.IsZombie))
-                {
-                    QStyleCast(Q,enemy,2);
-                }
-            }
             if (useW && W.IsReady())
             {
                 foreach (var enemy in HeroManager.Enemies.Where(o => o.IsValidTarget(W.Range) && !o.IsDead && !o.IsZombie))
@@ -371,19 +161,6 @@ namespace Kindred___YinYang
                             E.Cast(enemy);
                         }
                     }
-                }
-            }
-        }
-        private static void RLogic()
-        {
-            var minHP = Config.Item("min.hp.for.r").GetValue<Slider>().Value;  
-            foreach (var ally in HeroManager.Allies.Where(o=> o.HealthPercent < minHP && !o.IsRecalling() && !o.IsDead && !o.IsZombie
-                && Kindred.Distance(o.Position) < R.Range && !o.InFountain()))
-            {
-                if (Config.Item("respite."+ally.CharData.BaseSkinName).GetValue<bool>() && Kindred.CountEnemiesInRange(1500) >= 1 
-                    && ally.CountEnemiesInRange(1500) >= 1)
-                {
-                    R.Cast(ally);
                 }
             }
         }
@@ -469,7 +246,7 @@ namespace Kindred___YinYang
                 foreach (var enemy in HeroManager.Enemies.Where(x => x.IsValidTarget(1500)  && x.IsValid && x.IsVisible && !x.IsDead && !x.IsZombie))
                 {
                     Drawing.DrawText(enemy.HPBarPosition.X, enemy.HPBarPosition.Y, menuItem5.Color,
-                                        string.Format("{0} Basic Attack = Kill", AaIndicator(enemy)));
+                                        string.Format("{0} Basic Attack = Kill", Helper.AaIndicator(enemy)));
                 }
             }
         }
