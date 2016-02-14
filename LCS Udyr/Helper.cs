372fbf5ca4b401675dd48b7142f8837d4efc7a62
Hikigaya <otakusekainet@gmail.com>
2016-02-14 05:36:00 +0200

    UDYR

---
+++ LCS Udyr/Helper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+
+namespace LCS_Udyr
+{
+    static class Helper
+    {
+        public static bool IsTiger(this Obj_AI_Hero unit)
+        {
+            return unit.HasBuff("udyrtigerpunch");
+        }
+        public static bool IsTurtle(this Obj_AI_Hero unit)
+        {
+            return unit.HasBuff("udyrturtleactivation");
+        }
+        public static bool IsBear(this Obj_AI_Hero unit)
+        {
+            return unit.HasBuff("udyrbearactivation");
+        }
+        public static bool IsPhoenix(this Obj_AI_Hero unit)
+        {
+            return unit.HasBuff("udyrphoenixactivation"); // udyrbearstuncheck
+        }
+        public static bool HasBearPassive(this Obj_AI_Hero unit)
+        {
+            return unit.HasBuff("udyrbearstuncheck"); // 
+        }
+    }
+}
